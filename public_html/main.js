11143ea4b7507c95fa235f9a6465175e8ea6456f
Ahmet OZKESEK <aozkesek@gmail.com>
2016-03-05 23:33:24 +0200

    websocket and its listeners are added.

--- public_html/main.js
+++ public_html/main.js
@@ -14,6 +14,8 @@
 var NETAS = NETAS||{};
 
 NETAS.Events = {
+	LOGGEDIN: "LOGGEDIN",
+	LOGGEDOUT: "LOGGEDOUT",
 	RINGING: "RINGING",
 	ANSWERED: "ANSWERED",
 	HANGUP: "HANGUP"
@@ -21,21 +23,58 @@ NETAS.Events = {
 	
 NETAS.AgentDesktop = {
 	
+	_J_LOGIN: {},
+	_J_LOGOUT: {},
+	_J_REGISTER_EVENT: {},
+	
 	_options: {
 		Debug: false,
-		HostName: "",
+		Url: "",
 //		Authorization: Basic atob("username:password")
 		AgentAuth: ""
 		
 	},
 
 	_listeners: [],
+	_webSocket: null,
+	_isLoggedIn: false,
 	
 	_debug: function(e) {
 		if (this._options.Debug)
 			console.log(e);
 	},
 
+	_assert: function(b,m) {
+		if (b)
+			throw "Assertion failed: " + m;  
+	},
+	
+	_bindws: function() {
+		var _this = this;
+	
+		this._webSocket.onclose = function(event){ _this._onClose(event); };
+		this._webSocket.onerror = function(event){ _this._onError(event); };
+		this._webSocket.onmessage = function(event){ _this._onMessage(event); };
+		this._webSocket.onopen = function(event){ _this._onOpen(event); };
+	},
+	
+	_onClose: function(event) {
+		console.log(event);
+		
+	},
+	
+	_onError: function(event) {
+		console.log(event);
+	},
+	
+	_onMessage: function(event) {
+		console.log(event);
+	},
+	
+	_onOpen: function(event) {
+		console.log(event);
+		this._webSocket.send(JSON.stringify({requestType: "LOGIN", requestData: { AgentAuth: this._options.AgentAuth }}));
+	},
 	
 	/*
 	 * public methods
@@ -85,6 +124,25 @@ NETAS.AgentDesktop = {
 				  function(e) {
 					  return e.event === event;
 				  }).callback;
+	},
+	
+	login: function() {
+		
+		this._assert(this._options.Url === undefined || this._options.Url === null, "Url is null.");
+		this._assert(this._options.AgentAuth === undefined || this._options.AgentAuth === null, "AgentAuth is null.");
+		
+		this._webSocket = new WebSocket(this._options.Url);
+		this._bindws();
+		
+		
+		
+	},
+	
+	logout: function() {
+		
+		this._assert(!this._isLoggedIn, "You are not logged in.");
+		this._webSocket.send("LOGOUT");
+		
 	}
 
 };

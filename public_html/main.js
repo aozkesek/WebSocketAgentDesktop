2561155b5fbd331d1f3731fe9ee310e0612ee01f
Ahmet OZKESEK <aozkesek@gmail.com>
2016-03-06 21:09:57 +0200

    still under heavy development, but almost finalized I guess.

--- public_html/main.js
+++ public_html/main.js
@@ -14,15 +14,28 @@
 var NETAS = NETAS||{};
 
 NETAS.Events = {
-	LOGGEDIN: "LOGGEDIN",
-	LOGGEDOUT: "LOGGEDOUT",
-	RINGING: "RINGING",
-	ANSWERED: "ANSWERED",
-	HANGUP: "HANGUP"
+	Connected: "Connected",
+	Disconnected: "Disconnected",
+	Loggedin: "Loggedin",
+	Loggedout: "Loggedout",
+	Ringing: "Ringing",
+	Answered: "Answered",
+	Hangup: "Hangup",
+	Error: "Error"
 };
+
+NETAS.States = {
+	Unknown: "Unknown",
+	Connected: "Connected",
+	Disconnected: "Disconnected",
+	Loggedin: "Loggedin",
+	Loggedout: "Loggedout"
+};
+
+
 	
 NETAS.AgentDesktop = {
-	
+
 	_J_LOGIN: {},
 	_J_LOGOUT: {},
 	_J_REGISTER_EVENT: {},
@@ -31,13 +44,14 @@ NETAS.AgentDesktop = {
 		Debug: false,
 		Url: "",
 //		Authorization: Basic atob("username:password")
-		AgentAuth: ""
+		AgentAuth: "",
+		AgentDN: ""
 		
 	},
 
 	_listeners: [],
 	_webSocket: null,
-	_isLoggedIn: false,
+	_state: NETAS.States.Unknown,
 	
 	_debug: function(e) {
 		if (this._options.Debug)
@@ -58,6 +72,17 @@ NETAS.AgentDesktop = {
 		this._webSocket.onopen = function(event){ _this._onOpen(event); };
 	},
 	
+	_fireEventCallback: function(eventName, event) {
+	
+		this._listeners
+				  .filter(function(l){
+						return l.event === eventName; 
+					}).forEach(function(listener) { 
+						listener.callback(event); 
+					});
+		
+	},
+	
 	_onClose: function(event) {
 		console.log(event);
 		
@@ -65,84 +90,114 @@ NETAS.AgentDesktop = {
 	
 	_onError: function(event) {
 		console.log(event);
+	
 	},
 	
 	_onMessage: function(event) {
 		console.log(event);
+		
+		//
+		if (event.data !== undefined) {
+			var _data = JSON.parse(event.data);
+			if (_data.requestType !== undefined) {
+				this._fireEventCallback(_data.requestType, _data);
+			}
+		}
+		
 	},
 	
 	_onOpen: function(event) {
 		console.log(event);
-		this._webSocket.send(JSON.stringify({requestType: "LOGIN", requestData: { AgentAuth: this._options.AgentAuth }}));
+		
+		this._fireEventCallback(NETAS.Events.Connected, event);
+		
 	},
 	
 	/*
 	 * public methods
 	 */
-	getOption: function(option) {
-		if (option === undefined || option === null)
-			return this._options;
 		
-		if (this._options.hasOwnProperty(option))
-			return this._options[option];
-	},
-	
-	setOption: function(option, value) {
-		
-		if (value === undefined) {
-			this._options = option;
+		getOption: function(option) {
+			if (option === undefined || option === null)
+				return this._options;
+
+			if (this._options.hasOwnProperty(option))
+				return this._options[option];
+		},
+
+		setOption: function(option, value) {
+
+			if (value === undefined) {
+				this._options = option;
+			}
+			else if (this._options.hasOwnProperty(option)) {
+				this._options[option] = value;
+			}
+
+		},
+
+		addListener: function(event, callback) {
+			if (!NETAS.Events.hasOwnProperty(event))
+				throw "Unknown event!  Enter a valid event name or use NETAS.Events attributes.";
+
+			this.removeListener(event);
+			this._listeners.push({event: event, callback: callback});
+
+			this._debug(this._listeners);
+
+		},
+
+		removeListener: function(event) {
+			this._listeners.filter(
+					  function(e) {
+						  return e.event === event;
+					  }).pop();
+
+			this._debug(this._listeners);
+
+		},
+
+		getListener: function(event) {
+			return this._listeners.filter(
+					  function(e) {
+						  return e.event === event;
+					  }).callback;
+		},
+
+		connect: function() {
+			this._assert(this._options.Url === undefined || this._options.Url === null, "Url is null.");
+			this._assert(this._options.AgentAuth === undefined || this._options.AgentAuth === null, "AgentAuth is null.");
+
+			this._webSocket = new WebSocket(this._options.Url);
+			this._bindws();
+
+		},
+
+		disconnect: function() {
+
+		},
+
+		login: function() {
+
+			this._webSocket.send(JSON.stringify({
+				requestType: "LOGIN", 
+				requestData: { 
+					AgentAuth: this._options.AgentAuth 
+				}}));
+
+		},
+
+		logout: function() {
+
+			this._assert(!this._isLoggedIn, "You are not logged in.");
+
+			this._webSocket.send(JSON.stringify({
+				requestType: "LOGOUT", 
+				requestData: { 
+					AgentAuth: this._options.AgentAuth 
+				}}));
+
 		}
-		else if (this._options.hasOwnProperty(option)) {
-			this._options[option] = value;
-		}
-		
-	},
 	
-	addListener: function(event, callback) {
-		if (!NETAS.Events.hasOwnProperty(event))
-			throw "Unknown event!  Enter a valid event name or use NETAS.Events attributes.";
-
-		this.removeListener(event);
-		this._listeners.push({event: event, callback: callback});
-
-		this._debug(this._listeners);
-
-	},
-
-	removeListener: function(event) {
-		this._listeners.filter(
-				  function(e) {
-					  return e.event === event;
-				  }).pop();
-
-		this._debug(this._listeners);
-
-	},
-
-	getListener: function(event) {
-		return this._listeners.filter(
-				  function(e) {
-					  return e.event === event;
-				  }).callback;
-	},
-	
-	login: function() {
-		
-		this._assert(this._options.Url === undefined || this._options.Url === null, "Url is null.");
-		this._assert(this._options.AgentAuth === undefined || this._options.AgentAuth === null, "AgentAuth is null.");
-		
-		this._webSocket = new WebSocket(this._options.Url);
-		this._bindws();
-		
-		
-		
-	},
-	
-	logout: function() {
-		
-		this._assert(!this._isLoggedIn, "You are not logged in.");
-		this._webSocket.send("LOGOUT");
-		
-	}
 
 };
